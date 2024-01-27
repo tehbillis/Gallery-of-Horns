f61c8666a2e2eb24dbcaca01d5dce4577b18d8f5
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2024-01-27 15:41:15 -0800

    Complete feature 2

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 function HornedBest(props) {
+  const [likes, setLikes] = useState(0);
+
+  const handleLikeClick = () => {
+    setLikes(likes + 1);
+  };
+
   return (
     <section>
       <h2>{props.title}</h2>
-      <img src={props.imageUrl} alt={props.description} title={props.title} />
+      <img src={props.imageUrl} alt={props.description} title={props.title} onClick={handleLikeClick}/>
       <p>{props.description}</p>
+      <p>Likes: {likes}</p>
     </section>
   );
 }
