286c73e6d8381f4398df1a88a90a8f50328893c0
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2024-01-26 20:18:57 -0800

    Complete Feature 1: display images

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -2,7 +2,7 @@ function HornedBest(props) {
   return (
     <section>
       <h2>{props.title}</h2>
-      <img src={props.imgeUrl} alt={props.description} title={props.title} />
+      <img src={props.imageUrl} alt={props.description} title={props.title} />
       <p>{props.description}</p>
     </section>
   );
